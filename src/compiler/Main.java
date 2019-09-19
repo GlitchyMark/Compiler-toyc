34c5737d561b30ac9af58b1233572358c282adf9
GeorgeConIV <constantinege@mymail.vcu.edu>
2019-09-19 00:27:28 -0400

    added an apology

--- src/compiler/Main.java
+++ src/compiler/Main.java
@@ -8,6 +8,17 @@ public class Main
 {
     public static void main(String[] args)
     {
+        /**
+         * I have forgotten that coding in school and coding
+         * in the industry are two very different things.
+         *
+         * I have been coding for this project as if I was still
+         * in the industry, which is not wise. I apologise for this
+         * and shall try to write more practical, less future oriented
+         * code moving forward.
+         *
+         * I apologise mark.
+         */
         FileManipulator fileMan = new FileManipulator();
         fileMan.initFileScanner();
         List<String> words = fileMan.getListOfTokensWhitespaceDelimited();
