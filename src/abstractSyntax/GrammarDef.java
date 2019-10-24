d02320ec2c02c397230f9293c1fc70cb30688c86
GeorgeConIV <constantinege@mymail.vcu.edu>
2019-10-24 00:24:22 -0400

    Not sure

--- src/abstractSyntax/GrammarDef.java
+++ src/abstractSyntax/GrammarDef.java
@@ -4,35 +4,31 @@ import parser.*;
 
 public abstract class GrammarDef {
     TCparser parser;
-    boolean error = false;
-    int errorpos;
+    String errorSrc;
     String errormsg;
     public GrammarDef(TCparser tcp)
     {
         parser = tcp;
         parseDefinition();
         System.out.println("[PARSER] " + toString());
-        checkError();
     }
 
     abstract void parseDefinition();
 
-    void checkError()
+    void reportError()
     {
-        if(error)//print error here
-        {
-            parser.printer.printStack();
-            for(int i = 0; errorpos > i; i++)
-                System.out.print(" ");
-            System.out.println("^ " + errormsg);
-            System.exit(-1);
-        }
+        //parser.printer.printStack();
+        System.out.println(errorSrc.trim());
+        for(int i = 0; parser.getCurrentLineLoc() > i; i++)
+            System.out.print(" ");
+        System.out.println("^ " + errormsg);
+        System.exit(-1);
     }
 
     public void logError(String msg)
     {
-        error = true;
-        errorpos = parser.printer.getStringLength();
+        errorSrc = parser.getCurrentLine();
         errormsg = msg;
+        reportError();
     }
 }
