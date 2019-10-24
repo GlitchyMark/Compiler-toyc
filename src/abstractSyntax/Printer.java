bfd67733eb757a1d2cdf64c5c104a87a126cdd08
MJ9001 <mark.johnston9001@gmail.com>
2019-10-24 13:33:53 -0400

    Worked on adding parse tree

--- src/abstractSyntax/Printer.java
+++ src/abstractSyntax/Printer.java
@@ -54,11 +54,22 @@ public class Printer {
         //assert (pos >= 0);
     }
 
+    class Line
+    {
+        Line(String ln, int spc)
+        {
+            spaces = spc;
+            line = ln;
+        }
+        int spaces;
+        String line;
+    };
+
     public void printStack()
     {
         //for(int i = 0; i < fileprinting.size(); i++)
         //    System.out.println(fileprinting.get(i));
-        String split[] = builder.split("\\(");
+        /*String split[] = builder.split("\\(");
 
         for(int i = 0; i < split.length-1; i++)
         {
@@ -80,6 +91,32 @@ public class Printer {
             split2[i] = split2[i] + ((i == split.length-2) ? "" : ")") + "\n";
         }
         for(int i = 0; i < split2.length; i++)
-        System.out.print(split2[i]);
+        System.out.print(split2[i]);*/
+
+        List<Line> strbuilder = new ArrayList<Line>();
+        strbuilder.add(new Line(builder, 0));
+
+        while(strbuilder.get(strbuilder.size()-1).line.contains("("))
+        {
+            String split[] = strbuilder.get(strbuilder.size()-1).line.split("\\(", 1);
+            strbuilder.get(strbuilder.size()-1).line = split[0] + "(";
+            strbuilder.add(new Line(split[1], strbuilder.get(strbuilder.size()-1).spaces+1));
+        }
+        int iterator = 0;
+        while(strbuilder.get(iterator).line.contains(")"))
+        {
+            String split[] = strbuilder.get(iterator).line.split("\\)", 1);
+            strbuilder.get(iterator).line = split[0] + ")";
+
+            strbuilder.add(iterator+1, new Line(split[1], strbuilder.get(iterator).spaces-1));
+            for(int i = iterator; i < strbuilder.size(); i++)
+                strbuilder.get(i).spaces--;
+            iterator++;
+        }
+        for(int i = 0; i < strbuilder.size(); i++) {
+            for (int j = 0; j < strbuilder.get(i).spaces; j++)
+                System.out.print("  ");
+            System.out.println(strbuilder.get(i).line);
+        }
     }
 }
