e20a65dfa9a43b886e47e0661a5d88bd1eb5e8aa
GeorgeConIV <constantinege@mymail.vcu.edu>
2019-11-12 17:22:44 -0500

    symbol table stuff

--- src/symTable/SymTable.java
+++ src/symTable/SymTable.java
@@ -3,12 +3,59 @@
  */
 package symTable;
 
+import java.util.HashMap;
+
 public class SymTable{
+
+    HashMap<String, Symbol> localSymTable = new HashMap<>();
+
+    SymTable owner;
+    boolean hasOwner;
+
     // Constructor
-    public SymTable() {};
+    public SymTable()
+    {
+        hasOwner = false;
+    }
+
+    public SymTable(SymTable owner)
+    {
+        this.owner = owner;
+        hasOwner = true;
+    }
+
     // Instance Methods
-    public Symbol insert(String id,int type) throws SymbolAlreadyDeclared { return null;};
-    public Symbol insert(String id) throws SymbolAlreadyDeclared { return null;};
-    public Symbol find(String id) throws SymbolNotFound { return null;};
-    public String toString() { return "";};
+    public Symbol insert(String id, String type) throws SymbolAlreadyDeclared
+    {
+        Symbol sym = new Symbol(id, type);
+        localSymTable.put(sym.getName(), sym);
+        return sym;
+    }
+
+    public Symbol insert(String id) throws SymbolAlreadyDeclared
+    {
+        Symbol sym = new Symbol(id);
+        localSymTable.put(sym.getName(), sym);
+        return sym;
+    }
+
+    public Symbol find(String id) throws SymbolNotFound
+    {
+        Symbol sym;
+        if(localSymTable.containsKey(id))
+            sym = localSymTable.get(id);
+        else if(hasOwner)
+            sym = owner.find(id);
+        else {
+            System.out.println("Symbol with ID: " + id + " does not exist in symbol table");
+            sym = new Symbol();
+        }
+            
+        return sym;
+    }
+
+    public String toString()
+    {
+        return "";
+    }
 }
--- src/symTable/Symbol.java
+++ src/symTable/Symbol.java
@@ -2,16 +2,69 @@ package symTable;
 public class Symbol
 {
     // Public Instance Variables
-    public String name;
-    public int type;
-    public int offset;
-    // Exceptions
-    // Constructors
+    private String name;
+    private String type;
+    private int offset;
+
     public Symbol(){}
-    public Symbol(String name){}
-    public Symbol(String name,int offset){}
-    public Symbol(String name,int offset, int type){}
+
+    public Symbol(String name)
+    {
+        this.name = name;
+    }
+
+    public Symbol(String name,int offset)
+    {
+        this.name = name;
+        this.offset = offset;
+    }
+
+    public Symbol(String name, String type)
+    {
+        this.name = name;
+        this.type = type;
+    }
+
+    public Symbol(String name,int offset, String type)
+    {
+        this.name = name;
+        this.offset = offset;
+        this.type = type;
+    }
+
+    //get n set
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getOffset()
+    {
+        return  offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     // Instance Methods
-    public boolean equals(Symbol item){return false;};
+    public boolean equals(Symbol item)
+    {
+        return false;
+    }
+
     public String toString(){return "";}
 }
